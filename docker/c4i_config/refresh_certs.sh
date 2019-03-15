cac22d81107c6ead027b88b8a135a3baafbc68d4
maartenplieger <maartenplieger@gmail.com>
2019-03-15 14:42:26 +0000

    Updated refresh certs

--- docker/c4i_config/refresh_certs.sh
+++ docker/c4i_config/refresh_certs.sh
@@ -2,48 +2,27 @@
 pushd `pwd`
 cd `dirname $0`
 
-
-
-
 wget "https://github.com/ESGF/esgf-dist/raw/master/installer/certs/esg-truststore.ts" -O  esg-truststore.ts
+rm -rf certificates && mkdir certificates
 
 echo "Putting cert for accounts.google.com"
-echo | openssl s_client -connect accounts.google.com:443 2>&1 | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p'  > accounts.google.com.pem
+echo | openssl s_client -connect accounts.google.com:443 2>&1 | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p'  > ./certificates/accounts.google.com.pem
 keytool -delete -alias accounts.google.com  -keystore esg-truststore.ts -storepass changeit > /dev/null
-keytool -import -v -trustcacerts -alias accounts.google.com -file accounts.google.com.pem -keystore esg-truststore.ts -storepass changeit -noprompt
+keytool -import -v -trustcacerts -alias accounts.google.com -file ./certificates/accounts.google.com.pem -keystore esg-truststore.ts -storepass changeit -noprompt
 
+echo "Putting cert for slcs.ceda.ac.uk"
+echo | openssl s_client -connect slcs.ceda.ac.uk:443 2>&1 | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p'  > ./certificates/slcs.ceda.ac.uk.pem
+keytool -delete -alias slcs.ceda.ac.uk  -keystore esg-truststore.ts -storepass changeit > /dev/null
+keytool -import -v -trustcacerts -alias slcs.ceda.ac.uk -file ./certificates/slcs.ceda.ac.uk.pem -keystore esg-truststore.ts -storepass changeit -noprompt
 
-# 
-# echo "Putting cert for ceda.ac.uk"
-# echo | openssl s_client -connect ceda.ac.uk:443 2>&1 | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p'  > ceda.ac.uk.pem
-# keytool -delete -alias ceda.ac.uk  -keystore esg-truststore.ts -storepass changeit > /dev/null
-# keytool -import -v -trustcacerts -alias ceda.ac.uk -file ceda.ac.uk.pem -keystore esg-truststore.ts -storepass changeit -noprompt
-# 
-# 
-# echo "Putting cert for slcs.ceda.ac.uk"
-# echo | openssl s_client -connect slcs.ceda.ac.uk:443 2>&1 | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p'  > slcs.ceda.ac.uk.pem
-# keytool -delete -alias slcs.ceda.ac.uk  -keystore esg-truststore.ts -storepass changeit > /dev/null
-# keytool -import -v -trustcacerts -alias slcs.ceda.ac.uk -file slcs.ceda.ac.uk.pem -keystore esg-truststore.ts -storepass changeit -noprompt
-# 
-# # echo "Putting cert for slcs.ceda.ac.uk:7512"
-# # echo | openssl s_client -connect slcs.ceda.ac.uk:7512 2>&1 | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p'  > slcs.ceda.ac.uk7512
-# # keytool -delete -alias slcs.ceda.ac.uk7512  -keystore esg-truststore.ts -storepass changeit > /dev/null
-# # keytool -import -v -trustcacerts -alias slcs.ceda.ac.uk7512 -file slcs.ceda.ac.uk7512 -keystore esg-truststore.ts -storepass changeit -noprompt
-# # 
-# echo "Putting cert for slcs1.ceda.ac.uk443"
-# echo | openssl s_client -connect slcs1.ceda.ac.uk:443 2>&1 | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p'  > slcs1.ceda.ac.uk443.pem
-# keytool -delete -alias slcs1.ceda.ac.uk443  -keystore esg-truststore.ts -storepass changeit > /dev/null
-# keytool -import -v -trustcacerts -alias slcs1.ceda.ac.uk443 -file slcs1.ceda.ac.uk443.pem -keystore esg-truststore.ts -storepass changeit -noprompt
-# # 
-# echo "Putting cert for slcs1.ceda.ac.uk7512"
-# echo | openssl s_client -connect slcs1.ceda.ac.uk:7512 2>&1 | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p'  > slcs1.ceda.ac.uk7512.pem
-# keytool -delete -alias slcs1.ceda.ac.uk7512  -keystore esg-truststore.ts -storepass changeit > /dev/null
-# keytool -import -v -trustcacerts -alias slcs1.ceda.ac.uk7512 -file slcs1.ceda.ac.uk7512.pem -keystore esg-truststore.ts -storepass changeit -noprompt
+rm ./certificates/accounts.google.com.pem
+rm ./certificates/slcs.ceda.ac.uk.pem
 
 ### Instal ESGF certs ###
 #rm -rf esg_trusted_certificates*
 curl -L https://raw.githubusercontent.com/ESGF/esgf-dist/master/installer/certs/esg_trusted_certificates.tar > esg_trusted_certificates.tar
 tar -xvf esg_trusted_certificates.tar > /dev/null
+
 mkdir -p certificates
 for file in esg_trusted_certificates/*.0;do 
   a=${file##*/}
